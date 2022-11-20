895bab8350b5fe7d0fab55a04763ad60cf00b374
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-20 18:17:53 +0100

    ✨ Add basic components

---
+++ src/infrastructure/componentes/footer/footer.tsx
@@ -0,0 +1,9 @@
+import styles from "../header/header.module.css";
+
+export function Footer() {
+    return (
+        <footer className={styles.header}>
+            <address>©Robots🤖 - Aldana Arcidiacono</address>
+        </footer>
+    );
+}
